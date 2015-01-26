7ffbfc163fb405fb2bfe72ffa4239bc95995f28c
Akshita Jha <zenith158@gmail.com>
2015-01-26 19:38:37 +0530

    Make signup page

--- NLP_VLabs/NLP/urls.py
+++ NLP_VLabs/NLP/urls.py
@@ -3,5 +3,7 @@ from NLP import views
 
 urlpatterns = patterns('',
    url(r'^login/', views.loginview, name='login')
+   url(r'^auth/', views.auth_and_login, name='auth_and_login'),
+   url(r'^signup/', views.sign_up_in, name='sign_up_in'),
 
 )
