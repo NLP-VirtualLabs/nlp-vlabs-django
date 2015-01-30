e051875de18b3b5e0a3643a470dcd1162c96f6fe
Akshita Jha <zenith158@gmail.com>
2015-01-30 12:35:32 +0530

    Delete old NLP app

--- NLP_VLabs/NLP/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from NLP import views
-
-urlpatterns = patterns('',
-   url(r'^login/', views.loginview, name='login')
-   url(r'^auth/', views.auth_and_login, name='auth_and_login'),
-   url(r'^signup/', views.sign_up_in, name='sign_up_in'),
-
-)
