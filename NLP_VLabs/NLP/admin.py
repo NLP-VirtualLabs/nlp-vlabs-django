157ef84a2c5abe28da11b20ad3049acb5204befc
Akshita Jha <zenith158@gmail.com>
2015-02-08 21:54:51 +0530

    Add Admin View for all models

--- NLP_VLabs/NLP/admin.py
+++ NLP_VLabs/NLP/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from NLP.models import User_Profile
+from NLP.models import Experiments
+from NLP.models import Experiment_Stage
+from NLP.models import User_Experiments
+from NLP.models import User_Experiment_Stage
+
 # Register your models here.
+admin.site.register(User_Profile)
+admin.site.register(Experiments)
+admin.site.register(Experiment_Stage)
+admin.site.register(User_Experiments)
+admin.site.register(User_Experiment_Stage)
